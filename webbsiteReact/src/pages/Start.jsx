35e936c7ca135d82513869d3b68e08dcd5e23f82
JuliB-dotcom <69867011+JuliaB-dotcom@users.noreply.github.com>
2021-10-10 20:17:13 +0200

    Updated images size and placement plus added a new image

--- webbsiteReact/src/pages/Start.jsx
+++ webbsiteReact/src/pages/Start.jsx
@@ -1,5 +1,6 @@
 import troja1 from "/src/images/troja1.jpeg"
 import troja2 from "/src/images/troja2.jpeg"
+import troja3 from "/src/images/troja3.jpeg"
 import '/src/Start.css'
 function Start() {
   return (
@@ -9,43 +10,63 @@ function Start() {
         <h2 className="quote">Reuse the past recycle the present save the future</h2>
       </div>
       
-      <div className="allProducts">
-      <div className="startProduct1">
+        <div className="flexProducts">
+          <div className="img1">
+            <img src={troja2} width="250" />
+          </div>
+          <div className="img2">
+          <img src={troja1}  height="200" />
+          <img src={troja1}  height="200" />
+        </div>
+          </div>
+      {/* <div>
         <img src={troja2} alt="img1" />
-      </div>
+          </div>
       <div className="startText1">
         <p>titel</p>
         <p>pris</p>
       </div>
 
-      <div className="startProduct2">
+      <div>
         <img src={troja1} alt="img2" />
-        </div>
+          </div>
       
       <div className="startText2">
         <p>titel</p>
         <p>pris</p>
       </div>
-      <div className="startProduct3">
+      <div>
         <img src={troja1} alt="img2" />
-        </div>
+          </div>
       <div className="startText3">
         <p>titel</p>
         <p>pris</p>
-        </div>
-
+        </div> */}
+       
         {/* <hr/> */}
           <div className="background">
-        <div className="startProduct4">
-          <img src={troja2} alt="img1" />
+        {/* <div className="startProduct4">
+          <img src={troja3} alt="img1" />
+        </div> */}
         </div>
         <div className="startText4">
           <p>titel</p>
           <p>pris</p>
         </div>
-          </div>
+        
         <div className="startProduct5">
           <img src={troja2} alt="img1" />
+          <img src={troja2} alt="img1" />
+          <img src={troja2} alt="img1" />
+        </div>
+        <div className="text5">
+        <div className="startText5">
+          <p>titel</p>
+          <p>pris</p>
+        </div>
+        <div className="startText5">
+          <p>titel</p>
+          <p>pris</p>
         </div>
         <div className="startText5">
           <p>titel</p>
