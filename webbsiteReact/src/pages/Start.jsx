97721565e9cd897093cab5b9a372266b07811070
JuliB-dotcom <69867011+JuliaB-dotcom@users.noreply.github.com>
2021-10-25 18:33:01 +0200

    began to add the right imgages and updated css

--- webbsiteReact/src/pages/Start.jsx
+++ webbsiteReact/src/pages/Start.jsx
@@ -1,6 +1,5 @@
 import troja1 from "/src/images/troja1.jpeg"
 import troja2 from "/src/images/troja2.jpeg"
-import troja3 from "/src/images/troja3.jpeg"
 import '/src/Start.css'
 
 
@@ -49,10 +48,10 @@ function Start() {
       
           <div className="img4">
         </div>
-        <div className="imgText4">
+        {/* <div className="imgText4">
           <p>titel</p>
           <p>pris</p>
-        </div>
+        </div> */}
         
         <div className="img5">
           <img src={troja2} alt="img1" width="120" />
