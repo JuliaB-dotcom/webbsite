cd0935cdc1233638ac8f5d4d0da912911254b7f1
JuliB-dotcom <69867011+JuliaB-dotcom@users.noreply.github.com>
2021-11-01 10:24:47 +0100

    Added the right images

--- webbsiteReact/src/pages/Start.jsx
+++ webbsiteReact/src/pages/Start.jsx
@@ -1,5 +1,7 @@
 import troja1 from "/src/images/troja1.jpeg"
 import troja2 from "/src/images/troja2.jpeg"
+import img1 from "/src/images/img1.jpeg"
+import img2 from "/src/images/img2.jpg"
 import '/src/Start.css'
 
 
@@ -24,25 +26,28 @@ function Start() {
           
           /> */}
           <div className="img1">
-            <img src={troja2} width="150" />
+            <img src={img1} width="300" />
           </div>
+          <div className="text1">
           <div className="imgText1">
             <p>titel</p>
             <p>pris</p>
+            </div>
           </div>
           <div className="img2">
-          <img src={troja1}  width="100" />
-          <img src={troja1}  width="100" />
+            <img src={img2} width="300" />
+          {/* <img src={troja1}  width="100" />
+          <img src={troja1}  width="100" /> */}
           </div>
           <div className="text2">
           <div className="imgText2">
             <p>titel</p>
             <p>pris</p>
           </div>
-          <div className="imgText2">
+          {/* <div className="imgText2">
             <p>titel</p>
             <p>pris</p>
-            </div>
+            </div> */}
             </div>
           </div>
       
@@ -54,9 +59,9 @@ function Start() {
         </div> */}
         
         <div className="img5">
-          <img src={troja2} alt="img1" width="120" />
-          <img src={troja2} alt="img1" width="120" />
-          <img src={troja2} alt="img1" width="120" />
+          <img src={troja2} alt="img1" width="200" />
+          <img src={troja2} alt="img1" width="200" />
+          <img src={troja2} alt="img1" width="200" />
         </div>
         <div className="text5">
         <div className="imgText5">
