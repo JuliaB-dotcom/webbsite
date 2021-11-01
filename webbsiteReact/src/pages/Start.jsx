051d78fc9bc358c1e6c9997a8eaa259d5ec2eee6
JuliB-dotcom <69867011+JuliaB-dotcom@users.noreply.github.com>
2021-11-01 11:56:53 +0100

    Added media screen size

--- webbsiteReact/src/pages/Start.jsx
+++ webbsiteReact/src/pages/Start.jsx
@@ -14,50 +14,27 @@ function Start() {
       </div>
       
         <div className="flexProducts">
-{/* 
-          <startProducts
-            imgUrl:"",
-            text1:,
-            imgUrl:"",
-            imgUrl:"",
-            text2:,
-            text3:
-          
-          
-          /> */}
-          <div className="img1">
-            <img src={img1} width="300" />
-          </div>
+          <img className="img1" src={img1} width="300" />
+          <img className="img2" src={img2} width="300" />
+        </div>
+
           <div className="text1">
           <div className="imgText1">
             <p>titel</p>
             <p>pris</p>
             </div>
           </div>
-          <div className="img2">
-            <img src={img2} width="300" />
-          {/* <img src={troja1}  width="100" />
-          <img src={troja1}  width="100" /> */}
-          </div>
+        
           <div className="text2">
           <div className="imgText2">
             <p>titel</p>
             <p>pris</p>
           </div>
-          {/* <div className="imgText2">
-            <p>titel</p>
-            <p>pris</p>
-            </div> */}
             </div>
-          </div>
       
           <div className="img4">
-        </div>
-        {/* <div className="imgText4">
-          <p>titel</p>
-          <p>pris</p>
-        </div> */}
-        
+          </div>
+      
         <div className="img5">
           <img src={troja2} alt="img1" width="200" />
           <img src={troja2} alt="img1" width="200" />
