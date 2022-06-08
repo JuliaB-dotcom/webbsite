6057604c8fbe76c126bb8014a2cdacb969d07b10
JuliB-dotcom <69867011+JuliaB-dotcom@users.noreply.github.com>
2022-06-08 21:31:24 +0200

    Starting to build up the site again due to Router v.6 problems

--- webbsiteReact/src/App.jsx
+++ webbsiteReact/src/App.jsx
@@ -13,53 +13,59 @@ import copyright from "/src/images/copy.png"
 import {
   BrowserRouter as Router,
   Route,
-  Link
+  Link,
+  Routes
 } from 'react-router-dom'
+
 
 function App() {
 
 
   return (
+    //<div className="baseDiv">
     <div className="baseDiv">
-      
-      <Router>
-       
+    <Router>
+    
+
           <nav class="mainMenu">
-          <h1 class="logo">Katarina.B</h1>
-          <div class="menuContainer">
-          <input type="checkbox" name="" id=""></input>
-            <div class="hamburgerLines">
-              <span class="line line1"></span>
-              <span class="line line2"></span>
-              <span class="line line3"></span>
-          </div>
-          </div>
-          
-            
+            <h1 class="logo">Katarina.B</h1>
+            <div class="menuContainer">
+              <input type="checkbox" name="" id=""></input>
+              <div class="hamburgerLines">
+                <span class="line line1"></span>
+                <span class="line line2"></span>
+                <span class="line line3"></span>
+              </div>
+            </div>
+
+
             <ul class="menuItems">
               <li><Link to="/">START</Link></li>
               <li><Link to="/shop">SHOP</Link></li>
               <li><Link to="/kontakt">KONTAKT</Link></li>
               <li><Link to="/om">OM</Link></li>
               <li><Link to="/galleri">GALLERI</Link></li>
-              </ul>
-           
-        </nav>
-        <div className="backgroundImg"></div>
+            </ul>
+
+          </nav>
+          <div className="backgroundImg"></div>
           <h2 className="quote">Reuse the past recycle the present save the future</h2>
-       
-        <main>
+      <Routes>
+     
           <Route path="/" exact component={Start} />
           <Route path="/shop" exact component={Shop} />
             <Route path="/om" exact component={Om} />
             <Route path="/galleri" exact component={Galleri} />
-        </main>
+      
+        
+        </Routes> 
         <footer>
-          <a href="https://www.instagram.com/katarina1958">Följ mig på Instagram<img className="instagram" src={insta}/></a>
+          <a href="https://www.instagram.com/katarina1958">Följ mig på Instagram<img className="instagram" src={insta} /></a>
           <p><a href="mailto:hege@example.com">katarina.solbacken@gmail.com</a></p>
           <p className="copyText"> <img className="copySymbol" src={copyright} width="15" /> Julia Berg</p>
         </footer>
-      </Router> 
+    </Router>
+     
     </div>
     
     )
@@ -67,5 +73,3 @@ function App() {
 
 
 export default App
-
-  
